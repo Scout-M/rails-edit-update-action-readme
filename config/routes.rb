2884c52adfac1c41b6acadafb5ebf0bbaf4c9c56
Amy Mangor <aemangor@gmail.com>
2021-06-08 11:48:22 -0400

    commit tree 0296a0f40cd3a420fa10b4bb4264a1be7967f126

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles, only: [:index, :show, :new, :create]
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'  
 end
+
+
+# could also have this in resources: resources :articles, only: [:index, :show, :new, :create, :edit, :update] 
+# this accomplishes the same thing in one line. But with a PUT not a PATCH
+# PUT is fine to use, but generally it is used when replacing the whole thing
+# PATCH is used for sending a set of changes to the resource
